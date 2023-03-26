0c9e51cb0c915936a2f49766a1721d2147c79008
gffice <114347535+gffice@users.noreply.github.com>
2023-03-26 14:57:36 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -11,7 +11,7 @@ fi
 #  exit 3
 #fi
 
-echo "### Install ngrok ###"
+echo "Install ngrok"
 
 #wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
@@ -20,7 +20,7 @@ chmod +x ./ngrok
 #echo "### Update user: $USER password ###"
 #echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
 
-echo "### Start ngrok proxy for 8080 port ###"
+echo "Start ngrok proxy for 8080 port"
 
 
 rm -f .ngrok.log
@@ -33,7 +33,7 @@ HAS_ERRORS=$(grep "command failed" < .ngrok.log)
 if [[ -z "$HAS_ERRORS" ]]; then
   echo ""
   #echo "=========================================="
-  echo "#请在浏览器打开 : $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\// /" | sed "s/:/:/")"
+  echo "# 请在浏览器打开 : $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\// /" | sed "s/:/:/")"
   echo ""
   echo "网站生成时间:$(date '+%Y-%m-%d %H:%M:%S')"
   echo "有效期:5小时"
