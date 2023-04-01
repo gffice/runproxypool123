1c09902cc5bcb1e6af42adcdbba2ec82c18f3c20
gffice <114347535+gffice@users.noreply.github.com>
2023-04-01 23:36:56 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,17 +10,24 @@ echo "检测到指定内容，即将执行命令。"
 sleep 10
 mkdir http
 cd http
-curl http://0.0.0.0:8080/
-curl http://0.0.0.0:8080/clash
-curl http://0.0.0.0:8080/clash/config
-curl http://0.0.0.0:8080/clash/proxies
-curl http://0.0.0.0:8080/shadowrocket
-curl http://0.0.0.0:8080/surge/config
-curl http://0.0.0.0:8080/surge
-curl http://0.0.0.0:8080/surge/proxies
-curl http://0.0.0.0:8080/ss/sub
-curl http://0.0.0.0:8080/sip002/sub
-curl http://0.0.0.0:8080/sip002/sub
-curl http://0.0.0.0:8080/vmess/sub
-curl http://0.0.0.0:8080/trojan/sub
+mkdir clash
+mkdir shadowrocket
+mkdir surge
+mkdir ss
+mkdir sip002
+mkdir vmess
+mkdir trojan
+curl -o index.html http://0.0.0.0:8080/
+curl -o ./clash/index.html http://0.0.0.0:8080/clash
+curl -o ./clash/config http://0.0.0.0:8080/clash/config
+curl -o ./clash/proxies http://0.0.0.0:8080/clash/proxies
+curl -o ./shadowrocket/index.html http://0.0.0.0:8080/shadowrocket
+curl -o ./surge/config http://0.0.0.0:8080/surge/config
+curl -o ./surge/index.html http://0.0.0.0:8080/surge
+curl -o ./surge/proxies http://0.0.0.0:8080/surge/proxies
+curl -o ./ss/sub http://0.0.0.0:8080/ss/sub
+curl -o ./sip002/sub http://0.0.0.0:8080/sip002/sub
+curl -o ./ssr/sub http://0.0.0.0:8080/ssr/sub
+curl -o ./vmess/sub http://0.0.0.0:8080/vmess/sub
+curl -o ./trojan/sub http://0.0.0.0:8080/trojan/sub
 killall runproxypool #运行的命令
