a2384d8e4af623a6de78ec7fb173a9a00bbe642d
gffice <114347535+gffice@users.noreply.github.com>
2023-04-01 18:06:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 while true
 do
-  if grep -Fxq "指定字符" 文件名.txt
+  if grep -Fxq "INFO Usablility checking done." runlog.txt
   then
-    运行的命令
-    killall wget
+    sleep 30
+    mkdir http
+    cd http
+    curl http://0.0.0.0:8080/
+    curl http://0.0.0.0:8080/clash
+    curl http://0.0.0.0:8080/clash/config
+    curl http://0.0.0.0:8080/clash/proxies
+    curl http://0.0.0.0:8080/shadowrocket
+    curl http://0.0.0.0:8080/surge/config
+    curl http://0.0.0.0:8080/surge
+    curl http://0.0.0.0:8080/surge/proxies
+    curl http://0.0.0.0:8080/ss/sub
+    curl http://0.0.0.0:8080/sip002/sub
+    curl http://0.0.0.0:8080/sip002/sub
+    curl http://0.0.0.0:8080/vmess/sub
+    curl http://0.0.0.0:8080/trojan/sub
+    killall runproxypool #运行的命令
   fi
   sleep 5 #每隔5秒检查一次文件
 done
